4f5a9e9b84e52b96d465a6191adb4a846db64351
Enrique Mingorance Cano <emingora@redhat.com>
2021-12-03 08:04:42 +0100

    BXMPROD-1558 npmRegistry taken from npm_config_argv (#3)
    * BXMSPROD-1558 documentation adapted
    
    * BXMPROD-1558 -- npm commands

--- README.md
+++ README.md
@@ -5,11 +5,15 @@ Helpers for treating npm lock files, like removing/replacing the registry from t
 
 Install from npmjs.org:
 
-    npm install -g lock-treatment-tool
+    `npm install -g @kie/lock-treatment-tool`
 
-or run using `npx`
+or run using `npx` or `exec`
 
-    npx -p lock-treatment-tool locktt
+    `npm exec @kie/lock-treatment-tool`
+    `npx @kie/lock-treatment-tool`
+
+> **_Note:_** In case you are behind a proxy or you need to use and internal registry please check [The `--registry` problem](#the-registry-problem)
+
 
 ## Current commands
 
@@ -48,6 +52,16 @@ or run using `npx`
 
   locktt
 
+## The `--registry` problem
+
+One of the problems `locktt` tries to solve is to be able to install npm packages behing a proxy by using your own registry, and let's suppose the first thing you do is to execute locktt by executing `npx` or `npm exec` like:
+
+`npm exec @kie/lock-treatment-tool`
+
+It will fail (in case you haven't define `@kie` for using your registry on .npmrc file) since `@kie/lock-treatment-tool` library is not installed (let's supposed that) and you are behind a proxy, so you decide to specify a `--registry=THE_URL_TO_YOUR_REGISTRY` expecting to install the library using your registry. Fine!! It now works!! but... due to the [`-- npm problem`](https://docs.npmjs.com/cli/v8/commands/npm-run-script#description) you are not able to specify `--registry` either for `npm` and `locktt` execution.
+We are glad to tell you this problems dissapears since locktt version >0.0.1, we decided to parse `process.env.npm_config_argv` arguments in case registry is not specified for locktt (`npm exec @kie/lock-treatment-tool -- --registry=THE_URL_TO_YOUR_REGISTRY`) but for npm (`npm exec @kie/lock-treatment-tool --registry=THE_URL_TO_YOUR_REGISTRY`).
+
+> **_Note:_** In case you are behind a proxy or you need to use and internal registry just add `@kie:registry=YOUR_NPM_REGISTRY_URL` (replace `YOUR_NPM_REGISTRY_URL` by yours) or even `@kie:registry=${NPM_REGISTRY_URL}` (being `NPM_REGISTRY_URL` the environment variable pointing to your npm registry, you can use any other variable name)
 
 ## `frontend-maven-plugin` example
 
@@ -68,37 +82,20 @@ or run using `npx`
           </configuration>
       </execution>
       <execution>
-          <id>npm install lock-treatment-tool and run-node</id>
-          <phase>initialize</phase>
-          <goals>
-              <goal>npm</goal>
-          </goals>
-          <configuration>
-              <arguments>install lock-treatment-tool --global-style --no-package-lock --no-save</arguments>
-          </configuration>
-      </execution>
-      <execution>
           <id>lock-treatment-tool execution</id>
           <phase>initialize</phase>
           <goals>
-              <goal>npm</goal>
+              <goal>npx</goal>
           </goals>
           <configuration>
-              <arguments>run env -- locktt</arguments>
+              <arguments>@kie/lock-treatment-tool</arguments>
           </configuration>
       </execution>
-      <execution>
-          <id>yarn install</id>
-          <goals>
-              <goal>yarn</goal>
-          </goals>
-          <configuration>
-              <arguments>install</arguments>
-          </configuration>
-      </execution>                            
   </executions>
 </plugin>
 ```
+
+> **_Note:_** In case you are behind a proxy or you need to use and internal registry please check [The `--registry` problem](#the-registry-problem)
 
 ### Restrictions
 
--- script/treat-lock-files.js
+++ script/treat-lock-files.js
@@ -21,24 +21,45 @@ const { argv } = require('yargs')
   .boolean(['p', 's'])
   .alias('r', 'registry')
   .nargs('r', 1)
-  .describe('r', 'sets the registry to replace the host from yarn.lock resolved field')
-  .example('$0 --registry=https://npmregistry.redhat.com', 'sets the registry just for the yarn.lock file')
+  .describe(
+    'r',
+    'sets the registry to replace the host from yarn.lock resolved field',
+  )
+  .example(
+    '$0 --registry=https://npmregistry.redhat.com',
+    'sets the registry just for the yarn.lock file',
+  )
   .alias('p', 'replacePackageLockRegistry')
   .nargs('p', 0)
-  .describe('p', 'replaces the package-lock.json registry instead of removing it')
-  .example('$0 --registry=https://npmregistry.redhat.com --replacePackageLockRegistry', 'sets the registry just for the yarn.lock file')
+  .describe(
+    'p',
+    'replaces the package-lock.json registry instead of removing it',
+  )
+  .example(
+    '$0 --registry=https://npmregistry.redhat.com --replacePackageLockRegistry',
+    'sets the registry just for the yarn.lock file',
+  )
   .alias('s', 'skipIntegrity')
   .nargs('s', 0)
   .describe('s', 'skips integrity removal')
-  .example('$0 --skipIntegrity', 'sets the registry just for the yarn.lock file')
+  .example(
+    '$0 --skipIntegrity',
+    'sets the registry just for the yarn.lock file',
+  )
   .alias('f', 'folder')
   .nargs('f', 1)
   .describe('f', 'sets the project folder')
-  .example('$0 --folder=/path/to/project', 'treats the lock files from `/path/to/project` folder')
+  .example(
+    '$0 --folder=/path/to/project',
+    'treats the lock files from `/path/to/project` folder',
+  )
   .alias('o', 'outputFolder')
   .nargs('o', 1)
   .describe('o', 'sets the output folder to save the lock files')
-  .example('$0 --outputFolder=/path/to/output/folder', 'saves the treated lock files to `/path/to/output/folder` folder')
+  .example(
+    '$0 --outputFolder=/path/to/output/folder',
+    'saves the treated lock files to `/path/to/output/folder` folder',
+  )
   .help('h')
   .alias('h', 'help')
   .epilog('copyright 2019');
@@ -47,13 +68,36 @@ const npmLock = require('../lib/treat-locks/npm.lock');
 const yarnLock = require('../lib/treat-locks/yarn.lock');
 const NpmOptions = require('../lib/treat-locks/npm.options');
 
+/**
+ * It gets the registry from the line command in case registry is defined before `--`
+ */
+function getRegistryFromNpmCommand() {
+  const original = process.env.npm_config_argv
+    && JSON.parse(process.env.npm_config_argv).original;
+  const registryArgument = original
+    ? original.find((e) => e.startsWith('--registry='))
+    : undefined;
+  if (registryArgument) {
+    const registry = registryArgument.trim().split('=');
+    const registryValue = registry[1];
+    console.log(
+      '--registry value taken from npm/yarn arguments',
+      registryValue,
+    );
+    return registryValue;
+  }
+  return undefined;
+}
+
 function run() {
   console.log('Treating fields...');
   const folderPath = argv.folder === undefined ? '.' : argv.folder;
   const outputFolderPath = argv.outputFolder === undefined ? folderPath : argv.outputFolder;
-  const npmOptions = new NpmOptions(argv.registry,
+  const npmOptions = new NpmOptions(
+    argv.registry || getRegistryFromNpmCommand(),
     argv.replacePackageLockRegistry,
-    argv.skipIntegrity);
+    argv.skipIntegrity,
+  );
   npmLock(folderPath, outputFolderPath, npmOptions);
   yarnLock(folderPath, outputFolderPath, npmOptions);
 }
